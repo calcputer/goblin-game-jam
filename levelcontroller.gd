cd4d1696fdfbcc061581350bb9e22ad4de3d1ea7
Patrick <shipat13@gmail.com>
2020-11-14 15:50:07 -0600

    added life bar

--- levelcontroller.gd
+++ levelcontroller.gd
@@ -1,9 +1,16 @@
 extends Node
 
+var brain = false
+var brainJuice = 1000
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
-	#get_node("Player").connect("brainMode", self, "_on_player_brainMode")
+	get_node("Player").connect("brainMode", self, "_on_player_brainMode")
 
-#func _on_player_brainMode(brainMode):
-	#print(brainMode)
+func _on_player_brainMode(brainMode):
+	brain = brainMode
+
+func _process(delta):
+	if brain:
+		brainJuice = brainJuice -1
+		(get_node("GUI/LifeBar") as ProgressBar).value = brainJuice
